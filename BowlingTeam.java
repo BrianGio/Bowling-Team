ef13c464f37f312725d817ea91a11df03afa29c7
BrianGio <38891888+BrianGio@users.noreply.github.com>
2018-09-17 13:07:43 -0400

    Add files via upload

---
+++ BowlingTeam.java
@@ -0,0 +1,24 @@
+public class BowlingTeam
+{
+	private String teamName;
+	private String[] members = new String[4];
+
+	//Mutator method for the team name//
+	public void setTeamName(String team)
+	{
+		teamName = team;
+
+	}
+	public String getTeamName()
+	{
+		return teamName;
+	}
+	public void setMember(int number, String name)
+	{
+		members[number] = name;
+	}
+	public String getMember(int number)
+	{
+		return members[number];
+	}
+}
